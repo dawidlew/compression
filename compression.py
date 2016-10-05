806a6aa88664949e294a7b1fe26350674706e135
dawidlew <dawid.lewandowicz@allegro.pl>
2016-10-05 11:45:09 +0200

    Seventh

--- compression.py
+++ compression.py
@@ -22,22 +22,20 @@ def read_file_content(path):
 
 def compression(path):
     content = read_file_content(path)
+    
     ll = []
     for line in content:
         sl = {}
         last = None
         line = line.strip()
-        for _, char in enumerate(line):
-          # print("char: '%s'" % str(char))
+        for i, char in enumerate(line):
           if last <> char:
-            ll.append(sl)
             sl = {}
-            sl[str(char)] = 1
+            ll.append(sl)
+            sl[(char)] = 1
           else:
-            sl[str(char)] += 1
-          # print(str(sl))
+            sl[(char)] += 1
           last = char
-        # print sl
 
     cleaning_set(ll, path)
 
@@ -46,16 +44,13 @@ def cleaning_set(ll, path):
     s = ''
     for v in ll:
         for kk, vv in v.items():
+            if vv == 1:
+                vv = ""
+            elif vv == 2:
+                vv = str(kk)
             s += str(kk) + str(vv)
-
-    # print len(s)
-    # return s
-
-    print 'Zysk z kompresji: %.02f %%' % (100 - ((len(s) * 1.0 / size(path)) * 100.0))
-
-
-    # def write_file_content():
-
+    print s
+    print 'Zysk z kompresji: %.02f%%' % (100 - ((len(s) * 1.0 / size(path)) * 100.0))
 
 
 if __name__ == "__main__":
@@ -71,4 +66,4 @@ if __name__ == "__main__":
     if not args.filepath:
         args.filepath = 'c:/moje/aaa/git_nauka/compression/test1.txt'
 
-    compression('test2')
+    compression(args.filepath)
